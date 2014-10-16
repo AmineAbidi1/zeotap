4b4afc816e69d5001c764c07296ef41f8d6351c1
Amine Abidi <med.amine.labidi@gmail.com>
2014-10-16 15:09:53 +0200

    more validations

--- app/models/mapper.rb
+++ app/models/mapper.rb
@@ -1,5 +1,6 @@
 #encoding:utf-8
 class Mapper < ActiveRecord::Base
   validates :string1, :string2, presence: true
-  validates_format_of :string1, :string2, :with => /\A[a-z]+\z/i
+  validates_format_of :string1, :string2, :with => /\A[0-9a-z]+\z/i
+  validates_length_of :string1, :string2, :maximum => 40
 end
